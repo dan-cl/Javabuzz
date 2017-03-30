cff98b475a1dd0319acde924911966ccaa7631c2
Daniel Clissold <dan@Daniels-MacBook-Pro.local>
2017-03-30 12:25:25 +0100

    FizzBuzz method working

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -9,6 +9,26 @@ describe ('Javabuzz', function() {
       expect(javabuzz.isDivisibleByThree(3)).toBe(true);
     });
 
+    it('divisible by 5', function() {
+      javabuzz = new Javabuzz ();
+      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
+    });
+
+    it('Fizz', function() {
+      javabuzz = new Javabuzz ();
+      expect(javabuzz.isFizz(3)).toBe('Fizz');
+    });
+
+    it('Buzz', function() {
+      javabuzz = new Javabuzz ();
+      expect(javabuzz.isBuzz(5)).toBe('Buzz');
+    });
+
+    it('FizzBuzz', function() {
+      javabuzz = new Javabuzz ();
+      expect(javabuzz.isFizzBuzz(15)).toBe('FizzBuzz');
+    });
+
   });
 
   describe('knows when a number is NOT', function() {
@@ -16,6 +36,11 @@ describe ('Javabuzz', function() {
     it('divisible by 3', function() {
       javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+    });
+
+    it('divisible by 5', function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByFive(1)).toBe(false);
     });
 
   });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,5 +1,41 @@
 var Javabuzz = function(){};
 
 Javabuzz.prototype.isDivisibleByThree = function (number) {
-  return (number % 3 === 0);
+  if (number % 3 === 0) {
+    return true
+  } else {
+    return false
+  };
+};
+
+Javabuzz.prototype.isDivisibleByFive = function (number) {
+  if (number % 5 === 0) {
+    return true
+  } else {
+    return false
+  };
+};
+
+Javabuzz.prototype.isFizzBuzz = function (number) {
+  if (this.isDivisibleByThree(number) && this.isDivisibleByFive(number)) {
+    return 'FizzBuzz'
+  } else {
+    this.isFizz(number)
+  };
+};
+
+Javabuzz.prototype.isFizz = function (number) {
+ if (this.isDivisibleByThree(number)) {
+   return 'Fizz'
+ } else {
+   this.isBuzz(number)
+ };
+};
+
+Javabuzz.prototype.isBuzz = function (number) {
+  if (this.isDivisibleByFive(number)) {
+    return 'Buzz'
+  } else {
+    return number
+  };
 };
